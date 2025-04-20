cf7e8a1aae3ae8c3185ad2866db1daee5bc0e78c
ndbecker <ndbecker2@gmail.com>
2025-04-20 15:46:13 -0400

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((python-mode
+  . ((python-shell-interpreter . "/home/nbecker/ml-gpu/.pixi/envs/default/bin/python")
+     (python-shell-interpreter-args . "-i")
+     (eglot-workspace-configuration
+      . ((:python
+          (:pythonPath . "/home/nbecker/ml-gpu/.pixi/envs/default/bin/python")))))))
