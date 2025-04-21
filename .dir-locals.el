a94046f892e287228463d61b812cdfabf45cf56f
ndbecker <ndbecker2@gmail.com>
2025-04-21 11:10:38 -0400

    More hacks to .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,15 +1,16 @@
 ((python-mode
-  . ((eval . (setenv "PYTHONNOUSERSITE" "1"))
-     (eval . (let* ((root (locate-dominating-file default-directory ".dir-locals.el"))
-                    (pixi-python (expand-file-name ".pixi/envs/default/bin/python" root))
-                    (pixi-bin (expand-file-name ".pixi/envs/default/bin" root)))
-               (setq-local python-shell-interpreter pixi-python)
+  . ((eval . (let* ((pixi-root (locate-dominating-file default-directory ".dir-locals.el"))
+                    (python-bin (expand-file-name ".pixi/envs/default/bin/python" pixi-root))
+                    (bin-dir    (expand-file-name ".pixi/envs/default/bin" pixi-root)))
+               ;; Set python shell
+               (setq-local python-shell-interpreter python-bin)
                (setq-local python-shell-interpreter-args "-i")
-               (setq-local exec-path (cons pixi-bin exec-path))
+               ;; Set PATH
+               (setq-local exec-path (cons bin-dir exec-path))
+               ;; Set Eglot LSP config
                (setq-local eglot-workspace-configuration
-                           `(:python (:pythonPath ,pixi-python)))
-               (add-hook 'hack-local-variables-hook
-                         (lambda ()
-                           (unless (get-buffer-process (current-buffer))
-                             (run-python (python-shell-parse-command) nil t)))
-                         nil t))))))
+                           `(:python (:pythonPath ,python-bin)))
+               ;; Set PYTHONNOUSERSITE in the local environment
+               (make-local-variable 'process-environment)
+               (push "PYTHONNOUSERSITE=1" process-environment)
+               (setenv "PYTHONNOUSERSITE" "1"))))))
