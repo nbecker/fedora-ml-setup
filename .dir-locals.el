5a473fce0c5a9c6adbd365037c256786e5c16620
ndbecker <ndbecker2@gmail.com>
2025-04-21 10:18:52 -0400

    Update to ignore any existing ~/.local/lib/pythonxyz

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,9 @@
      (python-shell-interpreter-args . "-i")
      (eglot-workspace-configuration
       . ((:python
-          (:pythonPath . "/home/nbecker/ml-gpu/.pixi/envs/default/bin/python")))))))
+          (:pythonPath . "/home/nbecker/ml-gpu/.pixi/envs/default/bin/python"))))
+     (eval . (setenv "PYTHONNOUSERSITE" "1"))
+     (eval . (setq-local exec-path
+                         (cons (expand-file-name ".pixi/envs/default/bin"
+                                                 (locate-dominating-file default-directory ".dir-locals.el"))
+                               exec-path))))))
