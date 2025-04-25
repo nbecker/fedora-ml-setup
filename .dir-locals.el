800b9af8ec58f57f67d95ae271cac00ac30d8be2
ndbecker <ndbecker2@gmail.com>
2025-04-25 13:15:47 -0400

    Update

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,16 +1,23 @@
 ((python-mode
-  . ((eval . (let* ((pixi-root (locate-dominating-file default-directory ".dir-locals.el"))
-                    (python-bin (expand-file-name ".pixi/envs/default/bin/python" pixi-root))
-                    (bin-dir    (expand-file-name ".pixi/envs/default/bin" pixi-root)))
-               ;; Set python shell
-               (setq-local python-shell-interpreter python-bin)
+  . ((eval . (let* ((pixi-root default-directory)
+                    (python-bin  (expand-file-name ".pixi/envs/default/bin/python" pixi-root))
+                    (pixi-bin    (expand-file-name ".pixi/envs/default/bin" pixi-root))
+                    (libdevice   (expand-file-name
+                                  ".pixi/envs/default/lib/python3.10/site-packages/nvidia/cuda_nvcc/nvvm/libdevice/libdevice.10.bc"
+                                  pixi-root)))
+               ;; use the Pixi python
+               (setq-local python-shell-interpreter     python-bin)
                (setq-local python-shell-interpreter-args "-i")
-               ;; Set PATH
-               (setq-local exec-path (cons bin-dir exec-path))
-               ;; Set Eglot LSP config
+               ;; make lint/format find the right tools
+               (setq-local exec-path (cons pixi-bin exec-path))
+               ;; tell Eglot / LSP which python to use
                (setq-local eglot-workspace-configuration
                            `(:python (:pythonPath ,python-bin)))
-               ;; Set PYTHONNOUSERSITE in the local environment
+               ;; hide ~/.local site-packages
                (make-local-variable 'process-environment)
                (push "PYTHONNOUSERSITE=1" process-environment)
-               (setenv "PYTHONNOUSERSITE" "1"))))))
+               (setenv "PYTHONNOUSERSITE" "1")
+               ;; make Dr.Jit find libdevice
+               (push (concat "DRJIT_LIBDEVICE_PATH=" libdevice)
+                     process-environment)
+               (setenv "DRJIT_LIBDEVICE_PATH" libdevice))))))
